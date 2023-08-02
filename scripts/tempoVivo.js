468282196bdd96ff96e75da50df1adea432b4fc9
patatiz <tormentavictor@gmail.com>
2023-08-02 00:33:44 -0300

    Arrumando script

--- scripts/tempoVivo.js
+++ scripts/tempoVivo.js
@@ -6,22 +6,29 @@ export default function initTempoVivo() {
     function submit(btn) {
       btn.preventDefault();
       const data = {
-        ano: document.getElementById("ano").value,
-        mes: document.getElementById("mes").value,
-        dia: document.getElementById("dia").value,
+        ano: document.getElementById("ano"),
+        mes: document.getElementById("mes"),
+        dia: document.getElementById("dia"),
       };
-      const dataNascimento = new Date(`${data.ano}-${data.mes}-${data.dia}`);
+
+      const dataNascimento = new Date(
+        `${data.ano.value}-${data.mes.value}-${data.dia.value}`
+      );
 
       const idade =
         (dataAtual - dataNascimento) / (1000 * 60 * 60 * 24 * 365.25);
+        
       if (idade) {
-        if (data.dia > 0 && data.dia <= 31) {
-          if (data.mes > 0 && data.mes <= 12) {
-            if (data.ano > 1920 && data.ano <= dataAtual.getFullYear()) {
-              let anos = parseInt(idade);
-              let meses = parseInt((idade - anos) * 12);
-              let dias = parseInt(
-                idade * 365.25 - anos * 365.25 + (meses / 12) * 365.25
+        if (data.dia.value > 0 && data.dia.value <= 31) {
+          if (data.mes.value > 0 && data.mes.value <= 12) {
+            if (
+              data.ano.value > 1920 &&
+              data.ano.value <= dataAtual.getFullYear()
+            ) {
+              const anos = parseInt(idade);
+              const meses = parseInt((idade - anos) * 12);
+              const dias = parseInt(
+                idade * 365.25 - (anos * 365.25 + (meses / 12) * 365.25)
               );
               const tempoVivo = document.querySelectorAll(".idade h1 span");
               tempoVivo[0].innerText = anos;
